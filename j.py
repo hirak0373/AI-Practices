4c23dc90c193f64f7511f1c1b9fc8cf79c776998
Hira <Hirak0373>
2019-07-28 16:25:42 +0500

    I am practicing

--- j.py
+++ j.py
@@ -12,3 +12,28 @@ for f,l in h.items():
     print(f,l)
 if 3 in h.values():
     print("good")
+try:
+    print("ss")
+#else:
+ #   print("chk")
+except:
+    print("cc")
+finally:
+    print("take")
+def ggf(r, f=22):
+    d = r + f
+    return d
+t=ggf(4,5)
+print(t)
+if True:
+    print("hahahah")
+elif False:
+    print("Noooo")
+def num(first,second,*third):
+    print(first,"\n",second,"\n",third)
+num(100,200,300,400,500,600)
+def num(first,second,**third):
+    print(first,"\n",second)
+    for k,v in third.items():
+        print(k,":",v)
+num(100,200,third=300,forth=400,fifth=500,sixth=600)
